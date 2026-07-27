bb5fec08e92ba035624c8cd77b8f104a370ab480
Subhrajit Mal <subhrajitmal2021@gmail.com>
2026-07-27 20:52:18 +0530

    Time: 1 ms (99.93%), Space: 46.5 MB (58.77%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,14 +1,9 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<Character>[] r = new HashSet[9];
-        HashSet<Character>[] c = new HashSet[9];
-        HashSet<Character>[] b = new HashSet[9];
+        boolean[][] r = new boolean[9][9];
+        boolean[][] c = new boolean[9][9];
+        boolean[][] b = new boolean[9][9];
 
-        for(int i=0; i<9; i++){
-            r[i] = new HashSet<>();
-            c[i] = new HashSet<>();
-            b[i] = new HashSet<>();
-        }
         for(int i=0 ;i<9; i++){
             for(int j=0; j<9; j++){
                 char ch = board[i][j];
@@ -16,15 +11,16 @@ class Solution {
                 if(ch == '.'){
                 continue;
                 }
+                int num = board[i][j] - '1';
                 int box = (i/3)*3+(j/3);
-                if((r[i].contains(ch)) || 
-                c[j].contains(ch) || 
-                b[box].contains(ch)){
+                if(r[i][num] || 
+                c[j][num] || 
+                b[box][num]){
                  return false;
                 }
-                r[i].add(ch);
-                c[j].add(ch);
-                b[box].add(ch);
+                r[i][num] = true;
+                c[j][num] = true;
+                b[box][num] = true;
             }
         }
         return true;
