cb48290fa0c1d75171e56c6634a3720ee1d9dd8c
Subhrajit Mal <subhrajitmal2021@gmail.com>
2026-07-28 12:17:56 +0530

    Time: 0 ms (100%), Space: 48.2 MB (85.56%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -2,9 +2,9 @@ class Solution {
     public int search(int[] nums, int target) {
        int left = 0;
        int right = nums.length-1;
-       if(nums[left] == target){
-        return left;
-       }
+    //    if(nums[left] == target){
+    //     return left;
+    //    }
        while(left <= right){
         int mid = (left+right)/2;
         if(nums[mid] == target){
