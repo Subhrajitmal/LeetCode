3c73514263e3931b6d754b99b411118173bd343f
Subhrajit Mal <subhrajitmal2021@gmail.com>
2026-08-18 11:42:57 +0530

    Time: 2 ms (72.63%), Space: 44.9 MB (69.12%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -3,24 +3,51 @@ class Solution {
         int n = nums.length;
 
         HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<=n-k; i++){
-            HashSet<Integer> set = new HashSet<>();
+        for(int num: nums){
+            map.put(num,map.getOrDefault(num,0)+1);
 
-            for(int j=i;j<i+k;j++){
-                set.add(nums[j]);
-            }
-            for(int num: set){
-                map.put(num, map.getOrDefault(num,0)+1);
-            }
+        //     for(int j=i;j<i+k;j++){
+        //         set.add(nums[j]);
+        //     }
+        //     for(int num: set){
+        //         map.put(num, map.getOrDefault(num,0)+1);
+        //     }
+        // }
+        //     int ans = -1;
+        //     for(int r : map.keySet()){
+        //         if(map.get(r)==1){
+        //             if(r > ans){
+        //                 ans = r;
+        //             }
+        //         }
+        //     }
+        // return ans;
         }
-            int ans = -1;
-            for(int r : map.keySet()){
-                if(map.get(r)==1){
-                    if(r > ans){
-                        ans = r;
-                    }
+        if (k == n) {
+            int maxval = -1;
+            for (int num : nums) {
+                maxval = Math.max(maxval, num);
+            }
+            return maxval;
+        }
+        if (k == 1) {
+            int maxval = -1;
+            for (int num : nums) {
+                if (map.get(num) == 1) {
+                    maxval = Math.max(maxval, num);
                 }
             }
+            return maxval;
+        }
+        int ans = -1;
+        if (map.get(nums[0]) == 1) {
+            ans = Math.max(ans, nums[0]);
+        }
+        if (map.get(nums[n - 1]) == 1) {
+            ans = Math.max(ans, nums[n - 1]);
+        }
+
         return ans;
+
     }
 }
