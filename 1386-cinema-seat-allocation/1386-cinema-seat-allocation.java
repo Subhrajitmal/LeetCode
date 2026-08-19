0744dd56c3748b12f607888aeafb2d340d3bdff6
Subhrajit Mal <subhrajitmal2021@gmail.com>
2026-08-19 09:04:39 +0530

    Time: 33 ms (30.26%), Space: 51.7 MB (76.05%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -1,9 +1,10 @@
 class Solution {
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
-        HashSet<String> set =new HashSet<>();
+        HashSet<Integer> set =new HashSet<>();
         HashSet<Integer> row= new HashSet<>();
         for(int[] seat: reservedSeats){
-            set.add(seat[0]+","+seat[1]);
+            int key = seat[0]*11 + seat[1];
+            set.add(key);
             row.add(seat[0]);
         }
         int ans = (n-row.size())*2;
@@ -12,19 +13,19 @@ class Solution {
             boolean middle = true;
             boolean right = true;
         for(int i=2; i<=5; i++){
-            if(set.contains(r+","+i)){
+            if(set.contains(r*11+i)){
                 left = false;
                 break;
             }
         }
             for(int i=4; i<=7; i++){
-            if(set.contains(r+","+i)){
+            if(set.contains(r*11+i)){
                 middle = false;
                 break;
             }
         }
             for(int i=6; i<=9; i++){
-            if(set.contains(r+","+i)){
+            if(set.contains(r*11+i)){
                 right = false;
                 break;
             }
